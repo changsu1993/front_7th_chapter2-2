b8361bae15768e39487e0df555b3859b0c62c31d
changsu1993 <csdp0106@gmail.com>
2025-11-19 10:55:18 +0900

    feat: createVNode 함수 구현

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,3 +1,17 @@
 export function createVNode(type, props, ...children) {
-  return {};
+  // children 배열을 평탄화 (flatten)
+  const flattenChildren = (arr) => {
+    return arr.reduce((acc, child) => {
+      if (Array.isArray(child)) {
+        return acc.concat(flattenChildren(child));
+      }
+      return acc.concat(child);
+    }, []);
+  };
+
+  return {
+    type,
+    props,
+    children: flattenChildren(children),
+  };
 }
